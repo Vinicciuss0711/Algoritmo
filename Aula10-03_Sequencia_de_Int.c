54a2b1e3a018560214a76121e4854f7b8ebdb998
Vinicciuss0711 <vini07112006@gmail.com>
2025-09-11 21:49:49 -0300

    Aula10-2

--- Aula10-03_Sequencia_de_Int.c
+++ Aula10-03_Sequencia_de_Int.c
@@ -7,5 +7,22 @@ Sequência de Inteiros
 */
 
 int main(){
-    
+    int I, C=0,X=0,M=0;
+
+    printf("Digite numeros inteiros ou digite 0 para encerrar:\n");
+
+    while(I!=0){
+    scanf("%d",&I);
+    if(I>M){
+        M=I;
+    }
+    X=I+X;
+    C++;
+    }
+    printf("O redultado e:");
+    printf("\n%d",C-1);
+    printf("\n%d",M);
+    printf("\n%d",X/(C-1));
+
+    return 0;
 }
