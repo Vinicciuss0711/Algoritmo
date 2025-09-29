40dc99dc177ca89876afaa7cad498147659485d6
Vinicciuss0711 <vini07112006@gmail.com>
2025-09-29 16:39:45 -0300

    Aula13-01

--- Aula13-01_Analisador_Numeros.c
+++ Aula13-01_Analisador_Numeros.c
@@ -6,10 +6,10 @@ Analisador de Numeros
 */
 
 #include <stdio.h>
-#define TAM 3
+#define TAM 10
 
 int main(){
-    int inteiro[TAM], maior=0, menor=999999999;
+    int inteiro[TAM], maior=0, menor=999999999, impares=0, pares=0, soma=0, media;
 
     printf("Digite 10 numeros inteiros: ");
 
@@ -22,34 +22,40 @@ int main(){
     for (int i = 0; i < TAM; i++){
         printf("%d",inteiro[i]);
         
+        //para não colocar a virgula no último número
         if(i!=TAM-1){
             printf(", ");
         }
 
+        //maiores e menores
         if(inteiro[i]>maior){
             maior=inteiro[i];
         }
-        
         if(inteiro[i]<menor){
             menor=inteiro[i];
         }
 
+        //pares e impares
+        if(inteiro[i]%2==0){
+            pares++;
+        } else{
+            impares++;
+        }
+
+        soma += inteiro[i]; //soma = soma + inteiro[i];
     }
 
-
+    media = soma/TAM;
     
     printf("\nO maior numero e: %d",maior);
 
     printf("\nO menor numero e: %d",menor);
 
+    printf("\nA quantidade de numeros pares e: %d",pares);
 
-
-
-
-
-
-
-
+    printf("\nA quantidade de numeros impares e: %d",impares);    
+    
+    printf("\nA media aritmetica dos numeros e: %d",media);
 
     return 0;
 }
---
+++ Aula13-02.c
@@ -0,0 +1,6 @@
+/*
+Vinícius Carneiro Carvalho
+Aula de Algoritmo e Programação Estruturada
+25/09/2025
+Analisador de Numeros
+*/
